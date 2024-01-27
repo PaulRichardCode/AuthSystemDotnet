82dce5c313cd19b730bed7366bfbd07c2a22e856
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2024-01-27 18:52:24 +0100

    All set. waiting for m gration

--- AuthSystemDotnet/Areas/Identity/Data/ApplicationUser.cs
+++ AuthSystemDotnet/Areas/Identity/Data/ApplicationUser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
 namespace AuthSystemDotnet.Areas.Identity.Data;
@@ -9,5 +10,12 @@ namespace AuthSystemDotnet.Areas.Identity.Data;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    [PersonalData]
+    [Column(TypeName = "NavChar(100)")]
+    public string FirstName {get; set;}
+
+    [PersonalData]
+    [Column(TypeName = "NavChar(100)")]
+    public string LastName { get; set; }
 }
 
