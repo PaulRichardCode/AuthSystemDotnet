f3c74b339811a2a347bda337f52e4f240df8a249
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2024-01-28 20:19:43 +0100

    hmjnn

--- AuthSystemDotnet/Program.cs
+++ AuthSystemDotnet/Program.cs
@@ -4,7 +4,7 @@ using AuthSystemDotnet.Data;
 using AuthSystemDotnet.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("AuthDbContextConnection") ?? throw new InvalidOperationException("Connection string 'AuthDbContextConnection' not found.");
-
+//creating instance in deoendency injection
 builder.Services.AddDbContext<AuthDbContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AuthDbContext>();
